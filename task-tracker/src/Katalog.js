866238a6041c2d223928bcf7b8c30d06f332469d
we1k0n <124617511+we1k0n@users.noreply.github.com>
2023-12-21 13:57:44 +0200

    Katalog test

--- task-tracker/src/Katalog.js
+++ task-tracker/src/Katalog.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HeaderWithAuth from "./componetns/HeaderWithAuth";
+import NavigationBar from "./componetns/NavigationBar";
 
 
 function Katalog (){
     return(
         <div>
             <HeaderWithAuth/>
+            <NavigationBar/>
         </div>
     )
 }
